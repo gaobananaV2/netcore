10ad393e2fc06e52c4e929fa4cb31738c80b9595
gaobananaV2 <gaobanana@163.com>
2016-06-14 18:15:59 +0800

    DDD2

---
+++ NetCoreSln/DDD2/Application/BlogApplicatioinService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class BlogApplicatioinService
+    {
+        //一个事务
+        public void createBlog(String blogName, String userId)
+        {
+            //User user = userRepository.userById(userId);
+            //Blog blog = user.createBlog(blogName);
+            //blogRepository.save(blog);
+        }
+    }
+}
---
+++ NetCoreSln/DDD2/Util.Data/CollectionOrientedUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domains.Aggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Data
+{
+    public interface CollectionOrientedUserRepository
+    {
+         void add(User user);
+         User userById(String userId);
+         List<User> allUsers();
+         void remove(User user);
+    }
+}
