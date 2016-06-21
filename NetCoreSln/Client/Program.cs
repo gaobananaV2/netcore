ff5e56bcd9ccec833c896d53d4978990ff0de7b2
gaobananaV2 <gaobanana@163.com>
2016-06-21 18:03:30 +0800

    api odata

---
+++ NetCoreSln/Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using Client.WebApiOdata.Models;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        // Get an entire entity set.
+        static void ListAllProducts(Default.Container container)
+        {
+            foreach (var p in container.Products)
+            {
+                Console.WriteLine("{0} {1} {2}", p.Name, p.Price, p.Category);
+            }
+        }
+
+        static void AddProduct(Default.Container container, Product product)
+        {
+            container.AddToProducts(product);
+            var serviceResponse = container.SaveChanges();
+            foreach (var operationResponse in serviceResponse)
+            {
+                Console.WriteLine("Response: {0}", operationResponse.StatusCode);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            // TODO: Replace with your local URI.
+            string serviceUri = "http://localhost:56744/";
+            var container = new Default.Container(new Uri(serviceUri));
+
+            var product = new  Product()
+            {
+                Name = "Yo-yo",
+                Category = "Toys",
+                Price = 4.95M
+            };
+
+            AddProduct(container, product);
+            ListAllProducts(container);
+        }
+    }
+}
---
+++ NetCoreSln/WebApiOdata/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+using WebApiOdata.Models;
+using System.Web.OData.Builder;
+using System.Web.OData.Extensions;
+
+namespace WebApiOdata
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: null,
+                model: builder.GetEdmModel());
+        }
+    }
+}
