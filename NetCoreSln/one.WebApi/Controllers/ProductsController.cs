066df7e9c91e0982b2bd3ff737ad7173b8798c79
Administrator <Administrator@DESKTOP-C0D62U6>
2016-06-26 00:39:18 +0800

    1

---
+++ NetCoreSln/one.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace one.WebApi.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        dynamic context;
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            //var product = context.Products.FirstOrDefault((p) => p.Id == id);
+            var product="";
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; } 
+    }
+}
