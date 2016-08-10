81fca6205034fa91e4cf167e83238ac1d567161b
gaobananaV2 <gaobanana@163.com>
2016-08-10 16:08:25 +0800

    20160810

---
+++ NetCoreSln/ReadWriteUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReadWriteUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
