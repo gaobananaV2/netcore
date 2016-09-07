8fe2d91aa2ba0ad90f3af1a488d662544fa3fb9f
gaobananaV2 <gaobanana@163.com>
2016-09-07 18:28:00 +0800

    override

--- NetCoreSln/TDD/Console/Program.cs
+++ NetCoreSln/TDD/Console/Program.cs
@@ -12,4 +12,25 @@ namespace Console
         {
         }
     }
+
+    class Person 
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType().Equals(this.GetType()) == false) return false;
+            Person tmp = null;
+            tmp = (Person)obj;
+            return this.Name.Equals(tmp.Name) && this.Age.Equals(tmp.Age);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Age.GetHashCode() + this.Name.GetHashCode();
+        }
+    }
 }
