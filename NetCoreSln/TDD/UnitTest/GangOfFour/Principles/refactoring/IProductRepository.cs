e49aaa2c19788ef8145bbac73e83d0d60479f08c
gaobananaV2 <gaobanana@163.com>
2016-09-21 15:04:03 +0800

    adapter

---
+++ NetCoreSln/TDD/UnitTest/GangOfFour/Principles/refactoring/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAndInject.GangOfFour.Principles.refactoring
+{
+    public interface IProductRepository
+    {
+        IList<Product> GetAllProductsIn(int categoryId);
+    }
+}
---
+++ NetCoreSln/TDD/UnitTest/GangOfFour/Structural/6  Adapter/MSVelocityCacheAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAndInject.GangOfFour.Structural.Adapter
+{
+    public class MSVelocityCacheAdapter : ICacheStorage
+    {
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Retrieve<T>(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store(string key, object data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
