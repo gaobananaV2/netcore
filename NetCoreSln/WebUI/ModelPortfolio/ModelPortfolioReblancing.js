538bf3134e7d58762afa9b433d01b36d34bc6abf
gaobananaV2 <gaobanana@163.com>
2016-11-02 17:38:36 +0800

    1102

---
+++ NetCoreSln/WebUI/ModelPortfolio/ModelPortfolioReblancing.js
@@ -0,0 +1,56 @@
+
+
+var ModelPortfolioBuilder_Reblancing1 = { 
+    initBinding:function (){ /* add remove*/ },  
+    publish: function (){}, 
+    showUI:function(ReblancingModel){},
+
+    removeInvestment: function (id){}, 
+    canRemoveInvestment: function (id){},   
+    addInvestment:function (){},    
+
+    providerName:"",
+    providerId:"",
+    modelPortfolioName:"",
+    modelPortfolioId:""
+}
+/*
+var ReblancingModel={};
+
+var KeyValueModel={};
+
+*/
+
+
+
+var ModelPortfolioBuilder_Reblancing2 = { 
+    initBinding:function (){ /* add remove*/ },  
+    publish: function (){}, 
+    showUI:function(ReblancingModel){},
+
+    removeInvestment: function (id){}, 
+    canRemoveInvestment: function (id){},   
+    addInvestment:function (){},    
+
+    providerName:"",
+    providerId:"",
+    modelPortfolioName:"",
+    modelPortfolioId:""
+}
+
+
+
+var ModelPortfolioBuilder_Reblancing3 = { 
+    initBinding:function (){ /* add remove*/ },  
+    publish: function (){}, 
+    showUI:function(ReblancingModel){},
+
+    removeInvestment: function (id){}, 
+    canRemoveInvestment: function (id){},   
+    addInvestment:function (){},    
+
+    providerName:"",
+    providerId:"",
+    modelPortfolioName:"",
+    modelPortfolioId:""
+}
