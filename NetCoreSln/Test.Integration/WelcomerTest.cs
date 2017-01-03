6062557a1275c8b86b556faee97f268a492c7f64
gaobananaV2 <gaobanana@163.com>
2017-01-03 16:04:31 +0800

    test hello world

---
+++ NetCoreSln/Test.Integration/WelcomerTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Integration
+{
+    [TestClass]
+    public class WelcomerTest
+    {
+        [TestMethod]
+        public void Should_Say_Hello_World()
+        {
+            Assert.AreEqual("Hello World", Welcomer.SayHello());
+        }
+    }
+
+    internal class Welcomer
+    {
+        internal static string SayHello()
+        {
+            //return string.Empty;
+            return "Hello World";
+        }
+    }
+}
