b4b6e325e8194d5ea6691be9e59437819036cc08
gaobananaV2 <gaobanana@163.com>
2017-01-05 15:47:17 +0800

    1

---
+++ NetCoreSln/ConsoleAppCore/Properties/PublishProfiles/CORE.Console-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'ef4f7de9-addd-473a-b7b1-c4f4165cf93b'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
